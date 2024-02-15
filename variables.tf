de631603441ac88fcbbb1d0af4cb8725cdad7bcb
Mahith Chigurupati <saimahith.chigurupati@gmail.com>
2024-02-15 00:07:39 -0500

    stable infra build for gcp gke

--- variables.tf
+++ variables.tf
@@ -22,8 +22,62 @@ variable "billing_account" {
 
 }
 
-variable "ssh_key" {
+variable "ssh_key_path" {
   description = "The ssh key"
   type        = string
+
+}
+
+
+variable "cidr_block" {
+  type = string
+}
+
+variable "k8s_pod_range" {
+  type = string
+}
+
+variable "k8s_service_range" {
+  type = string
+
+}
+
+variable "ssh_username" {
+  type = string
+
+}
+
+variable "region" {
+  type = string
+
+}
+
+variable "master_ipv4_cidr_block" {
+  type = string
+
+}
+
+variable "jenkins_cidr_block" {
+  type = string
+
+}
+
+variable "min_node_count" {
+  type = number
+
+}
+
+variable "max_node_count" {
+  type = number
+
+}
+
+variable "node_machine_type" {
+  type = string
+
+}
+
+variable "ssh_private_key" {
+  type = string
 
 }
